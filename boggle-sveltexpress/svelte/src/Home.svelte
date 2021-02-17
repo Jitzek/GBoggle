5a73c835ef1788aa4b82585ee5778d122009eead
AnnoyingDog <jan.vlasman99@gmail.com>
2021-02-17 11:34:18 +0100

    BasicBackground+Uploadbutton
    - Created BasicBackground component
    - Created UploadButton component
    - Added multiple svg icons as components
    - Added HomePage

---
+++ boggle-sveltexpress/svelte/src/Home.svelte
@@ -0,0 +1,12 @@
+<script>
+    import UploadButton from "./components/UploadButton.svelte";
+    import CameraSvg from "./components/svg/camera.svelte";
+    import SoundbarSvg from "./components/svg/soundbars.svelte"
+</script>
+<div>
+    <h1>Welcome Home!</h1>
+    <!-- Uploadbutton: use image/* and audio/* -->
+    <UploadButton acceptedfiletypes="audio/*"><SoundbarSvg color="white"/></UploadButton>
+</div>
+<style>
+</style>
---
+++ boggle-sveltexpress/svelte/src/components/BasicBackground.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<div class="basicbackground">
+    <slot></slot>
+</div>
+
+<style>
+.basicbackground{
+    background-color: rgba(255, 255, 255, 0.400);
+}
+</style>
