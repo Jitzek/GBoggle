5df6c3af5c96df99c34f70cd69220b17406629e8
AnnoyingDog <jan.vlasman99@gmail.com>
2021-02-19 19:32:33 +0100

    joepiedepoepie

---
+++ boggle-sveltexpress/svelte/src/components/svg/flagNL.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let width: string = "75px";
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 6" width="{width}">
+    <rect fill="#21468B" width="9" height="6" />
+    <rect fill="#FFF" width="9" height="4" />
+    <rect fill="#AE1C28" width="9" height="2" />
+</svg>
+<style>
+    svg {
+        margin: 0 auto;
+        display: block;
+    }
+</style>
