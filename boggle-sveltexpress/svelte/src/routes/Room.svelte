2d0837ec4b5b6a1a1e9003417524b58090d18467
Jitzek <jitz_k@hotmail.com>
2021-03-02 18:44:31 +0100

    Primary markup and styling

--- boggle-sveltexpress/svelte/src/routes/Room.svelte
+++ boggle-sveltexpress/svelte/src/routes/Room.svelte
@@ -1,73 +1,142 @@
 <script lang="ts">
-    import BasicContainer from "../components/BasicContainer.svelte";
-    import Text from "../components/Text.svelte";
+  import BasicContainer from "../components/BasicContainer.svelte";
+  import GboggleLogo from "../components/GBoggleLogo.svelte";
+  import Text from "../components/Text.svelte";
+  import { SelectInput } from "../components/inputs";
+  import LinkButton from "../components/LinkButton.svelte";
+  import Shuttle from "../components/svg/shuttle.svelte";
+
+  export let id: string;
+
+  let inviteLink: HTMLElement;
+
+  /**
+   * 
+   * @param e Element to be copied
+   */
+  function copyElement(e: HTMLElement) {
+    // Remember display style
+    let temp = e.style.display;
+
+    // Make sure display is not 'none'
+    e.style.display = "initial";
+
+    let selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+        selection.removeAllRanges();
+    }
+
+    let range = document.createRange();
+    range.selectNode(e);
+    selection.addRange(range);
+    document.execCommand('copy');
+
+    // Reset display style
+    e.style.display = temp;
+  }
 </script>
 
 <div>
-    
-    <BasicContainer>
-        <Text fontSize="2.5rem" value="Room Settings" />
-    </BasicContainer>
+  <GboggleLogo size="20rem" />
+  <BasicContainer>
+    <Text fontSize="2.5rem" value="Room Settings" />
+    <form>
+      <SelectInput label="Rounds" name="rounds">
+        {#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as round}
+          <option value="{round}">{round}</option>
+        {/each}
+      </SelectInput>
+      <div style="margin-top: 2rem;"></div>
+      <SelectInput label="Round time in seconds" name="time">
+        {#each [30, 60, 90, 120, 150, 180, 210, 240, 270, 300] as time}
+          <option value="{time}">{time}</option>
+        {/each}
+      </SelectInput>
+      <div style="margin-top: 2rem;"></div>
+      <SelectInput label="Language" name="language">
+        {#each ["Dutch"] as language}
+          <option value="{language}">{language}</option>
+        {/each}
+      </SelectInput>
+      <div style="margin-top: 6rem;"></div>
+      <LinkButton
+        btn_background="#13a8e0"
+        btn_width="100%"
+        text_color="white"
+        value="Start Game"
+      >
+        <Shuttle color="#13a8e0" width="60%" />
+      </LinkButton>
+      <div style="padding-bottom: 4rem;"></div>
+    </form>
+  </BasicContainer>
 
-    <div style="margin-top: 4rem"></div>
+  <div style="margin-top: 2rem"></div>
 
-    <Text fontSize="2.5rem" value="Invite Others!" />
-    <div class="invite-container">
-        <div class="invite-link-container">
-            <span class="hover-me">Hover over here to see the invite link!</span>
-            <span class="invite-link">Invite Link</span>
-        </div>
-        <div class="hover-btn-container">
-            <button class="hover-btn">
-                <Text  value="Copy" />
-            </button>
-        </div>
+  <Text fontSize="2.5rem" value="Invite Others!" />
+  <div class="invite-container">
+    <div class="invite-link-container">
+      <span id="link" class="hover-me"
+        >Hover over here to see the invite link!</span
+      >
+      <span bind:this={inviteLink} on:click="{() => copyElement(inviteLink)}" class="invite-link">{`${window.location.host}/room/${id}`}</span>
     </div>
+    <div class="hover-btn-container">
+      <button class="hover-btn" on:click="{() => copyElement(inviteLink)}">
+        <Text value="Copy" />
+      </button>
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
-    .invite-container {
-        margin: auto;
-        text-align: center;
-        vertical-align: middle;
-        display: table;
-    }
+  form {
+    max-width: 80%;
+    margin: auto;
+  }
 
-    .hover-btn {
-        background: rgb(255, 136, 0);;
-        border: none;
-        margin: 0 !important;
-        padding: 0.5rem;
-        cursor: pointer;
-    }
+  .invite-container {
+    margin: auto;
+    text-align: center;
+    vertical-align: middle;
+    display: table;
+  }
 
-    .hover-btn:active {
-        background: rgb(255, 123, 0);
-    }
+  .hover-btn {
+    background: rgb(255, 136, 0);
+    border: none;
+    margin: 0 !important;
+    padding: 0.5rem;
+    cursor: pointer;
+  }
 
-    .hover-btn-container {
-        display: table-cell;
-    }
+  .hover-btn:active {
+    background: rgb(255, 123, 0);
+  }
 
-    .invite-link-container {
-        background: white;
-        color: rgb(255, 136, 0);
-        width: 100%;
-        display: table-cell;
-    }
+  .hover-btn-container {
+    display: table-cell;
+  }
 
-    .invite-link-container:hover {
-        .invite-link {
-            display: initial;
-        }
+  .invite-link-container {
+    background: white;
+    color: rgb(255, 136, 0);
+    width: 100%;
+    display: table-cell;
+  }
 
-        .hover-me {
-            display: none;
-        }
-    }
-
+  .invite-link-container:hover {
     .invite-link {
-        display: none;
-        color: black;
+      display: initial;
     }
+
+    .hover-me {
+      display: none;
+    }
+  }
+
+  .invite-link {
+    display: none;
+    color: black;
+  }
 </style>
